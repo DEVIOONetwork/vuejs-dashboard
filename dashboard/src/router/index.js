7ba376875b34873d449688dd0ef542a2c949465b
Pronicio <52421988+Pronicio@users.noreply.github.com>
2022-01-10 15:34:29 +0100

    Vue router history mode.

--- dashboard/src/router/index.js
+++ dashboard/src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHashHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router'
 
 const routes = [
   {
@@ -44,7 +44,8 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHashHistory(),
+  mode: 'history',
+  history: createWebHistory(),
   routes
 })
 
